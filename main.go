2b6b57de57df5e4de67fd935f0ecce2331d278e5
ignaciomosca <ignaciomosca@outlook.com>
2020-01-30 18:12:29 -0300

    First web server

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>Hello World</h1>")
+}
 
 func main() {
-	fmt.Println("Hello World")
+	http.HandleFunc("/", handlerFunc)
+	fmt.Println("Running on port 3000")
+	http.ListenAndServe(":3000", nil)
 }
