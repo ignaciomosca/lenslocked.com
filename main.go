723df835f6f3ee29ee354ac6fce5dcd1f4ad767a
ignaciomosca <ignaciomosca@outlook.com>
2020-02-24 18:38:33 -0300

    Add signin and signup forms

--- main.go
+++ main.go
@@ -13,6 +13,8 @@ var (
 	contactTemplate  *views.View
 	faqTemplate      *views.View
 	notFoundTemplate *views.View
+	signInTemplate   *views.View
+	signUpTemplate   *views.View
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -30,6 +32,16 @@ func faq(w http.ResponseWriter, r *http.Request) {
 	must(faqTemplate.Render(w, nil))
 }
 
+func signIn(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	must(signInTemplate.Render(w, nil))
+}
+
+func signUp(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	must(signUpTemplate.Render(w, nil))
+}
+
 func notFound(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	notFoundTemplate.Template.ExecuteTemplate(w, notFoundTemplate.Layout, nil)
@@ -42,11 +54,15 @@ func main() {
 	faqTemplate = views.NewFiles("bootstrap", "views/faq.gohtml")
 	contactTemplate = views.NewFiles("bootstrap", "views/contact.gohtml")
 	notFoundTemplate = views.NewFiles("bootstrap", "views/notfound.gohtml")
+	signInTemplate = views.NewFiles("bootstrap", "views/sign-in.gohtml")
+	signUpTemplate = views.NewFiles("bootstrap", "views/sign-up.gohtml")
 
 	template.New("blah")
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
 	r.HandleFunc("/faq", faq)
+	r.HandleFunc("/sign-in", signIn)
+	r.HandleFunc("/sign-up", signUp)
 	r.NotFoundHandler = http.HandlerFunc(notFound)
 	fmt.Println("Running on port 3000")
 	http.ListenAndServe(":3000", r)
