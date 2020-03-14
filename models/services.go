f5ac96df8d8aa158088ef7ac04187dc7901ab68f
Ignacio Mosca <ignaciomosca@outlook.com>
2020-03-14 11:36:26 -0300

    Add Sevices struct

---
+++ models/services.go
@@ -0,0 +1,19 @@
+package models
+
+import (
+	"github.com/jinzhu/gorm"
+)
+
+func NewServices(connectionInfo string) (*Services, error) {
+	db, err := gorm.Open("postgres", connectionInfo)
+	if err != nil {
+		return nil, err
+	}
+	db.LogMode(true)
+	return &Services{}, nil
+}
+
+type Services struct {
+	User    UserService
+	Gallery GalleryService
+}
