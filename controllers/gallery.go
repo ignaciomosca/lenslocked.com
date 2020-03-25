6b21f92cfee8e27c8628a31340864920cfdfa3b6
Ignacio Mosca <ignaciomosca@outlook.com>
2020-03-24 21:45:11 -0300

    Updated gallery service. Added validators and update logic

--- controllers/gallery.go
+++ controllers/gallery.go
@@ -85,7 +85,16 @@ func (g *Galleries) Update(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	gallery.Title = form.Title
-	//g.GalleryService.Update
+	err = g.GalleryService.Update(gallery)
+	if err != nil {
+		vd.SetAlert(err)
+		g.EditView.Render(w, vd)
+		return
+	}
+	vd.Alert = &views.Alert{
+		Level:   views.AlertLvlSuccess,
+		Message: "Gallery succesfully updated",
+	}
 	g.EditView.Render(w, vd)
 }
 
--- models/galleries.go
+++ models/galleries.go
@@ -4,6 +4,12 @@ import (
 	"github.com/jinzhu/gorm"
 )
 
+const (
+	ErrUserIDRequired   modelError = "models: user ID is required"
+	ErrTitleRequired    modelError = "models: title is required"
+	ErrGalleryIDInvalid modelError = "models: ID provided was invalid"
+)
+
 // Gallery is our image container resources that visitors view
 type Gallery struct {
 	gorm.Model
@@ -17,6 +23,7 @@ type GalleryService interface {
 type GalleryDB interface {
 	ById(id uint) (*Gallery, error)
 	Create(gallery *Gallery) error
+	Update(gallery *Gallery) error
 }
 
 // ById lookups a gallery by id.
@@ -46,6 +53,47 @@ func NewGalleryService(db *gorm.DB) GalleryService {
 	}
 }
 
+func (gv galleryValidator) Create(gallery *Gallery) error {
+	err := runGalleryValFns(gallery,
+		gv.userIDRequired,
+		gv.titleRequired)
+	if err != nil {
+		return err
+	}
+	return gv.GalleryDB.Create(gallery)
+}
+
+func (gv galleryValidator) Update(gallery *Gallery) error {
+	err := runGalleryValFns(gallery,
+		gv.userIDRequired,
+		gv.titleRequired)
+	if err != nil {
+		return err
+	}
+	return gv.GalleryDB.Update(gallery)
+}
+
+func (gv *galleryValidator) userIDRequired(g *Gallery) error {
+	if g.UserID <= 0 {
+		return ErrUserIDRequired
+	}
+	return nil
+}
+
+func (gv *galleryValidator) titleRequired(g *Gallery) error {
+	if g.Title == "" {
+		return ErrTitleRequired
+	}
+	return nil
+}
+
+func (gv *galleryValidator) nonZeroID(gallery *Gallery) error {
+	if gallery.ID <= 0 {
+		return ErrGalleryIDInvalid
+	}
+	return nil
+}
+
 type galleryService struct {
 	GalleryDB
 }
@@ -59,4 +107,19 @@ var _ GalleryDB = &galleryGorm{}
 func (gg *galleryGorm) Create(gallery *Gallery) error {
 	gg.db.AutoMigrate(&Gallery{})
 	return gg.db.Create(gallery).Error
+}
+
+func (gg *galleryGorm) Update(gallery *Gallery) error {
+	return gg.db.Save(gallery).Error
+}
+
+type galleryValFn func(*Gallery) error
+
+func runGalleryValFns(gallery *Gallery, fns ...galleryValFn) error {
+	for _, fn := range fns {
+		if err := fn(gallery); err != nil {
+			return err
+		}
+	}
+	return nil
 }
